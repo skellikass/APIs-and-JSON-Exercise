b6644c2dfae58358dd1c2ba9625b3ab58db4cf75
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2023-01-06 15:43:16 -0500

    uncommented out Ron and Kanye

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -10,36 +10,38 @@ namespace APIsAndJSON
     {
         static void Main()
         {
+            #region OpenWeatherAPI
             OpenWeatherMapAPI weather = new OpenWeatherMapAPI();
             var currentWeather = weather.GetCurrentWeather();
             Console.WriteLine(currentWeather);
+            #endregion
 
-            //#region Ron and Kanye
-            //QuoteGenerator quoteGen = new QuoteGenerator();
+            #region Ron and Kanye
+            QuoteGenerator quoteGen = new QuoteGenerator();
 
-            //Console.WriteLine("It was a balmy evening one mid-January in the year 2530 when a Mr. Ron Swanson \n" +
-            //    "and a Mr. Kanye West found themselves in the same room.  They decided to converse, \n" +
-            //    "and this is what transpired...");
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //Console.WriteLine($"Ron said to Kanye, {quoteGen.RonQuoteGenerator()} \n" +
-            //    "\n" +
-            //    $"To which Kanye said, \"{quoteGen.KanyeQuoteGenerator()}\"");
-            //Console.WriteLine();
-            //Console.WriteLine($"Ron then responded, {quoteGen.RonQuoteGenerator()} \n" +
-            //    "\n" +
-            //    $"Contemplating this, Kanye replied, \"{quoteGen.KanyeQuoteGenerator()}\"");
-            //Console.WriteLine();
-            //Console.WriteLine($"Shocked by this, Ron said, {quoteGen.RonQuoteGenerator()} \n" +
-            //    "\n" +
-            //    $"Kanye then announced, \"{quoteGen.KanyeQuoteGenerator()}\"");
-            //Console.WriteLine();
-            //Console.WriteLine($"Finally, Ron ended the conversation with {quoteGen.RonQuoteGenerator()} \n" +
-            //    "\n" +
-            //    $"Kanye, needing to get the last word in, said, \"{quoteGen.KanyeQuoteGenerator()}\"");
-            //Console.WriteLine();
-            //Console.WriteLine("The End");
-            //#endregion
+            Console.WriteLine("It was a balmy evening one mid-January in the year 2530 when a Mr. Ron Swanson \n" +
+                "and a Mr. Kanye West found themselves in the same room.  They decided to converse, \n" +
+                "and this is what transpired...");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Ron said to Kanye, {quoteGen.RonQuoteGenerator()} \n" +
+                "\n" +
+                $"To which Kanye said, \"{quoteGen.KanyeQuoteGenerator()}\"");
+            Console.WriteLine();
+            Console.WriteLine($"Ron then responded, {quoteGen.RonQuoteGenerator()} \n" +
+                "\n" +
+                $"Contemplating this, Kanye replied, \"{quoteGen.KanyeQuoteGenerator()}\"");
+            Console.WriteLine();
+            Console.WriteLine($"Shocked by this, Ron said, {quoteGen.RonQuoteGenerator()} \n" +
+                "\n" +
+                $"Kanye then announced, \"{quoteGen.KanyeQuoteGenerator()}\"");
+            Console.WriteLine();
+            Console.WriteLine($"Finally, Ron ended the conversation with {quoteGen.RonQuoteGenerator()} \n" +
+                "\n" +
+                $"Kanye, needing to get the last word in, said, \"{quoteGen.KanyeQuoteGenerator()}\"");
+            Console.WriteLine();
+            Console.WriteLine("The End");
+            #endregion
         }
     }
 }
